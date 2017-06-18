5b2e38989c2f1f0a91a59da7dda96ae07a211c60
Elías Portugal Luna <portugal402@gmail.com>
2017-06-18 17:18:22 -0500

    Check for a Internet connection

--- background.sh
+++ background.sh
@@ -3,11 +3,14 @@
 NOW="$(date +'%m-%d-%Y.%H.%M.%S')"
 PIC="$NOW.jpg"
 USER= "USER"
-wget -O "$HOME/.local/share/backgrounds/$PIC"  https://source.unsplash.com/user/$USER/likes/1920x1080
+wget -q --spider http://google.com
+if [ $? -eq 0 ]; then
+  wget -O "$HOME/.local/share/backgrounds/$PIC"  https://source.unsplash.com/user/$USER/likes/1920x1080
 
-NEWNAME=$(stat -c%s "$HOME/.local/share/backgrounds/$PIC")
+  NEWNAME=$(stat -c%s "$HOME/.local/share/backgrounds/$PIC")
 
-cd $HOME/.local/share/backgrounds/
-mv "$PIC" "$NEWNAME.jpg"
+  cd $HOME/.local/share/backgrounds/
+  mv "$PIC" "$NEWNAME.jpg"
 
-gsettings set org.gnome.desktop.background picture-uri "file://$HOME/.local/share/backgrounds/$NEWNAME.jpg"
+  gsettings set org.gnome.desktop.background picture-uri "file://$HOME/.local/share/backgrounds/$NEWNAME.jpg"
+fi
